7be93339f97be9dc650c4555d56a3e1bfa7954ae
zhanxiaoma <1049778092@qq.com>
2023-05-07 17:10:21 +0800

    项目初始化

---
+++ src/main/java/cn/work/work_prepare/WorkPrepareApplication.java
@@ -0,0 +1,13 @@
+package cn.work.work_prepare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkPrepareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkPrepareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/work/work_prepare/WorkPrepareApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.work.work_prepare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkPrepareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
